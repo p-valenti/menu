3eb628439305871ae64ce4c77d59edc1140c86e0
Valentina Popova <solval89@gmail.com>
2022-11-18 08:12:19 +0200

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div style={{background: "grey"}}>
+      <b>Your food</b>
+      <div>
+        <ul>
+          <li>Menu</li>
+          <li>Recipes</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
