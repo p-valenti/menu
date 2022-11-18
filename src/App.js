445823f890f4a1163f851eabad38ab535f6c87d7
Valentina Popova <solval89@gmail.com>
2022-11-18 08:20:59 +0200

    Add component for content for first page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { StartPage } from "./components/StartPage";
 
 function App() {
   return (
     <>
     <Header />
+    <StartPage />
     </>
   );
 }
---
+++ src/components/StartPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const StartPage = () => {
+  return (
+    <div style={{background: "green"}}>
+        StartPage
+    </div>
+  )
+}
