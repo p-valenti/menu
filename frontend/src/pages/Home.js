f2f838f5b52fd698e31aefd5188b16a432fee629
Valentina Popova <solval89@gmail.com>
2022-12-07 22:18:01 +0200

    Fetch

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [menus, setMenus] = useState(null)
+    useEffect(() => {
+        const fetchMenus = async () => {
+            const response = await fetch('http://localhost:4000/api/menus')
+            const json = await response.json()
+            if (response.ok) {
+                setMenus(json)
+            }
+        }
+        fetchMenus()
+    }, [])
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="menus">
+                {menus && menus.map((menu) => (
+                    <p key={menu.date}>{menu.date}</p>
+                ))}
+            </div>
         </div>
     )
 }
