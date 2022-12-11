89c771f2ad80ba283cb3051c0101943bfc3deec5
Valentina Popova <solval89@gmail.com>
2022-12-11 22:42:26 +0200

    Setting the Initial Auth Status

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export const Navbar = () => {
-  const { logout } = useLogout()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
   const handleClick = () => {
-    logout()
-  }
+    logout();
+  };
   return (
     <header>
-      <div className='container'>
+      <div className="container">
         <Link to="/">
           <h1>Food menu</h1>
         </Link>
         <nav>
-          <div>
-            <button onClick={handleClick}>Log out</button>
-          </div>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </div>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -15,6 +15,12 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { 
     user: null
   })
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'))
+    if (user) {
+      dispatch({ type: 'LOGIN', payload: user })
+    }
+  }, [])
   console.log('AuthContext state:', state)
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
