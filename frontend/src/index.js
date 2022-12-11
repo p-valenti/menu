3d8f828be29b802bac50534b34525114d12d4856
Valentina Popova <solval89@gmail.com>
2022-12-11 21:35:18 +0200

    React Auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { MenusContextProvider } from './context/MenuContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { MenusContextProvider } from "./context/MenuContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //     <React.StrictMode>
@@ -12,11 +13,13 @@ import { MenusContextProvider } from './context/MenuContext'
 //     </React.StrictMode>
 // )
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <MenusContextProvider>
-            <App />
-        </MenusContextProvider>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <AuthContextProvider>
+      <MenusContextProvider>
+        <App />
+      </MenusContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
